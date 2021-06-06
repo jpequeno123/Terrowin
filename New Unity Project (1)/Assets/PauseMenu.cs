c2e15c56561fcfe10ef49edd539ab56e1eec95cc
DxT69 <84232007+DxT69@users.noreply.github.com>
2021-06-06 20:31:18 +0100

    20:31

--- New Unity Project (1)/Assets/PauseMenu.cs
+++ New Unity Project (1)/Assets/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -34,6 +35,11 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Options Menu");
     }
     public void QuitGame()
     {
